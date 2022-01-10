53903f357c5194574eb600d65ceddbc9dbedce09
Fabián Álvarez <fa.alvarez@uc.cl>
2022-01-10 13:08:01 -0300

    Added a docstring

--- color_translator.py
+++ color_translator.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 def color_translator(color):
+	"""The color_translator function receives the name of a color, then prints its hexadecimal value.
+	Currently, it only supports the three additive primary colors (red, green, blue), so it returns
+	"unknown" for all other colors."""
 	if color == "red":
 		hex_color = "#ff0000"
 	elif color == "green":
@@ -11,6 +14,7 @@ def color_translator(color):
 		hex_color = "unknown"
 	return hex_color
 
+print(help(color_translator))
 print(color_translator("blue")) # Should be #0000ff
 print(color_translator("yellow")) # Should be unknown
 print(color_translator("red")) # Should be #ff0000
