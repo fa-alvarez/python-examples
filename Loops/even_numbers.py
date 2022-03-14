27a6dfe913584eb0eac5eb4d3d5f7872b6381b9f
Fabián Álvarez <fa.alvarez@uc.cl>
2022-03-13 23:56:40 -0300

    Add even_numbers.py

---
+++ Loops/even_numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def even_numbers(maximum):
+	"""The even_numbers function returns a space-separated string of all positive
+	numbers that are divisible by 2, up to and including the maximum that's passed
+	into the function. For example, even_numbers(6) returns “2 4 6”."""
+	return_string = ""
+	for x in range(2, maximum + 1, 2):
+		return_string += str(x) + " "
+	return return_string.strip()
+
+print(even_numbers(6))  # Should be 2 4 6
+print(even_numbers(10)) # Should be 2 4 6 8 10
+print(even_numbers(1))  # No numbers displayed
+print(even_numbers(3))  # Should be 2
+print(even_numbers(0))  # No numbers displayed
