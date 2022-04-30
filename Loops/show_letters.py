f6fac369ee0e3cde4e04d099a4a52eea71835b61
Fabián Álvarez <fa.alvarez@uc.cl>
2022-04-29 23:39:19 -0400

    Add show_letters.py

---
+++ Loops/show_letters.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# The show_letters function should print out each letter of a word on a separate
+# line. Fill in the blanks to make that happen.
+
+def show_letters(word):
+	for x in word:
+		print(x)
+
+show_letters("Hello")
+# Should print one line per letter
