e43882280d4f4c700cfe5f7a6c6e5528946e1b0c
Fabián Álvarez <fa.alvarez@uc.cl>
2022-05-27 23:31:04 -0400

    Add flower.py

---
+++ OOP/flower.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class Flower():
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "red"
+
+violet = Flower()
+violet.color = "blue"
+
+this_pun_is_for_you = "The honey is sweet and so are you"
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you)
