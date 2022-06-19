f055b99ee20a52a94558bfb5f94a435a46d7541b
Fabián Álvarez <fa.alvarez@uc.cl>
2022-06-18 23:47:27 -0400

    Add is_power_of.py

---
+++ Recursion/is_power_of.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    if number == 1:
+      return True
+    else:
+      return False
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number / base, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
