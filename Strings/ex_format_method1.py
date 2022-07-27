0196be855515042812cf3308200464c844decb3f
Fabián Álvarez <fa.alvarez@uc.cl>
2022-07-27 12:22:39 -0400

    Add ex_format_method1.py

---
+++ Strings/ex_format_method1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# This is an example of using the format() method on a string
+
+example = "format() method"
+
+formatted_string = "this is an example of using the {} on a string".format(example)
+
+print(formatted_string)
+
+"""Outputs:
+this is an example of using the format() method on a string
+"""
