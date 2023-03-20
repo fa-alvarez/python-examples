d8f773b45dfa325542dd25ad35643007af273685
Fabián Álvarez <faalvare@udec.cl>
2023-03-19 23:08:01 -0300

    Add another example.

--- Recursion/is_power_of.py
+++ Recursion/is_power_of.py
@@ -15,3 +15,4 @@ def is_power_of(number, base):
 print(is_power_of(8,2)) # Should be True
 print(is_power_of(64,4)) # Should be True
 print(is_power_of(70,10)) # Should be False
+print(is_power_of(144,12)) # Should be True
